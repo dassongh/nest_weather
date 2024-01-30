d72f9f8a0911586043964049485caa9a96fc4846
Alexander S <oleksandr.slv@gmail.com>
2024-01-30 22:27:18 +0200

    add open-weather module;

--- src/modules/open-weather/open-weather.service.ts
+++ src/modules/open-weather/open-weather.service.ts
@@ -1,6 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+import { WeatherResponse } from './interfaces';
 
 @Injectable()
 export class OpenWeatherService {
-  constructor() {}
+  private apiKey: string;
+  private baseUrl: string;
+
+  constructor(configService: ConfigService) {
+    this.apiKey = configService.get<string>('OPEN_WEATHER_API_KEY');
+    this.baseUrl = configService.get<string>('OPEN_WEATHER_BASE_URL');
+  }
+
+  getByLatLng(latitude: number, longitude: number, exclude?: string[]): Promise<WeatherResponse> {
+    let url = `${this.baseUrl}?lat=${latitude}&lon=${longitude}&appid=${this.apiKey}`;
+
+    if (exclude) {
+      url += `&exclude=${exclude.join(',')}`;
+    }
+
+    return fetch(url).then(res => res.json());
+  }
 }
