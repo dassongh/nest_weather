796a061ab2356d5ba615b01a946aaffcd73e1332
Alexander S <oleksandr.slv@gmail.com>
2024-01-31 23:02:39 +0200

    remove unused dependencies;

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { DatabaseExceptionFilter } from './common/database-exception.filter';
 
@@ -10,10 +9,6 @@ async function bootstrap() {
   app.setGlobalPrefix('/api/v1');
   app.useGlobalFilters(new DatabaseExceptionFilter());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-
-  const config = new DocumentBuilder().setTitle('NestJS').setDescription('NestJS API').setVersion('1.0').build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
